198ca45a148e737f5a4fcb6b4ef99c65d5ba940b
Benjamin Vandevelde <benjamin_vandevelde@hotmail.com>
2020-08-14 17:07:29 +0200

    Add project files.

---
+++ 2DArrays/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace _2DArrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.tbToevoegen = new System.Windows.Forms.TextBox();
+            this.btnToevoegen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(0, 0);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(35, 13);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "label1";
+            // 
+            // tbToevoegen
+            // 
+            this.tbToevoegen.Location = new System.Drawing.Point(3, 16);
+            this.tbToevoegen.Name = "tbToevoegen";
+            this.tbToevoegen.Size = new System.Drawing.Size(100, 20);
+            this.tbToevoegen.TabIndex = 1;
+            // 
+            // btnToevoegen
+            // 
+            this.btnToevoegen.Location = new System.Drawing.Point(3, 42);
+            this.btnToevoegen.Name = "btnToevoegen";
+            this.btnToevoegen.Size = new System.Drawing.Size(75, 23);
+            this.btnToevoegen.TabIndex = 2;
+            this.btnToevoegen.Text = "Voeg toe";
+            this.btnToevoegen.UseVisualStyleBackColor = true;
+            this.btnToevoegen.Click += new System.EventHandler(this.btnToevoegen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 657);
+            this.Controls.Add(this.btnToevoegen);
+            this.Controls.Add(this.tbToevoegen);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox tbToevoegen;
+        private System.Windows.Forms.Button btnToevoegen;
+    }
+}
+
---
+++ 2DArrays/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2DArrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<string> dagLijst = new List<string>() { "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag" };
+        private string[,] mijnAgenda = new string[24 ,7];
+        public int uur, dag;
+        public void lb_Click(object sender,EventArgs e)
+        {
+            string[] separators = { ","};
+            string value = (sender as Label).Name;
+            string[] split = value.Split(separators,StringSplitOptions.RemoveEmptyEntries);
+            uur = Convert.ToInt32(split[0]);
+            dag = Convert.ToInt32(split[1]);
+            Zichtbaar();
+        }
+        private void btnToevoegen_Click(object sender, EventArgs e)
+        {
+            mijnAgenda[uur,dag] = tbToevoegen.Text;
+            Label actiefLabel = this.Controls.Find($"{uur},{dag}",true).FirstOrDefault() as Label;
+            actiefLabel.Text = mijnAgenda[uur, dag];
+            Onzichtbaar();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mijnAgenda[17,2 ] = "test";
+            VulAgenda();
+            Onzichtbaar();
+        }
+        private void Onzichtbaar() 
+        {
+            lblMessage.Location = new Point(10, 10);
+            lblMessage.Text = "Selecteer een uur en dag.";
+            tbToevoegen.Clear();
+            btnToevoegen.Hide();
+            tbToevoegen.Hide();
+            btnToevoegen.Enabled = false;
+            tbToevoegen.Enabled = false;
+        }
+        private void Zichtbaar() 
+        {
+            lblMessage.Text = ($"Geef een nieuwe taak voor {dagLijst[dag]} om {uur.ToString()} uur: ");
+            btnToevoegen.Show();
+            tbToevoegen.Show();
+            btnToevoegen.Enabled = true;
+            tbToevoegen.Enabled = true;
+            tbToevoegen.Location = new Point(10 + lblMessage.Width+10,10);
+            btnToevoegen.Location = new Point(10 + lblMessage.Width + 10 + tbToevoegen.Width + 10, 10);
+        }
+        private void VulAgenda()
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                var newTextBox = new TextBox();
+                newTextBox.Width = 70;
+                newTextBox.Height = 20;
+                newTextBox.Enabled = false;
+                newTextBox.Location = new Point(50, 50 + (i+1) * newTextBox.Height);
+                newTextBox.Text = $"{i.ToString()}:00";
+                Controls.Add(newTextBox);
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                var newTextBox = new TextBox();
+                newTextBox.Width = 70;
+                newTextBox.Height = 20;
+                newTextBox.Enabled = false;
+                newTextBox.Location = new Point(50 + (i + 1) * newTextBox.Width,50);
+                newTextBox.Text = dagLijst[i];
+                Controls.Add(newTextBox);
+            }
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 24; j++)
+                {
+                    var newLabel = new Label();
+                    newLabel.Width = 70;
+                    newLabel.Height = 20;
+                    newLabel.Location = new Point(120 + newLabel.Width * i, 70 + newLabel.Height * j);
+                    newLabel.Text = mijnAgenda[j, i];
+                    newLabel.Name = $"{j},{i}";
+                    newLabel.IsAccessible = false;
+                    newLabel.BorderStyle = BorderStyle.FixedSingle;
+                    newLabel.Click += new EventHandler(lb_Click);
+                    Controls.Add(newLabel);
+                }
+            }
+        }
+    }
+}
